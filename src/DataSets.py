90278c2a15b19db2bac8950b7bc30ba6009f3037
Martin Neumayer <neumayer@isse.de>
2020-05-26 08:52:30 +0200

    Allow rotation of Parts

--- src/DataSets.py
+++ src/DataSets.py
@@ -7,7 +7,7 @@ class DataSets(object):
     @staticmethod
     def get_demo_parts():
         return [
-            Part(22000, 5800), Part(22000, 5800), Part(5620, 5600), Part(5620, 5600), Part(5620, 5600),
+            Part(5800, 22000), Part(22000, 5800), Part(5620, 5600), Part(5620, 5600), Part(5620, 5600),
             Part(22000, 5740), Part(5775, 5960), Part(12340, 5960), Part(5960, 3725)
         ]
 
