ecaa969a2d1f237b0627c1f40bdaec7fec025cdb
Martin Neumayer <neumayer@isse.de>
2020-06-03 09:30:04 +0200

    Add more demo parts

--- src/PartData.py
+++ src/PartData.py
@@ -1,3 +1,5 @@
+import itertools
+
 from Part import Part
 
 
@@ -12,7 +14,10 @@ class PartData(object):
 
     @staticmethod
     def get_demo_parts_2():
-        return PartData.get_hochschrank_1() + PartData.get_oberschrank_1() + PartData.get_oberschrank_2()
+        return PartData.get_hochschrank_1() \
+               + PartData.get_oberschrank_1() \
+               + PartData.get_unterschrank_4() \
+               + PartData.get_traverse(10)
 
     @staticmethod
     def get_hochschrank_1():
@@ -34,3 +39,15 @@ class PartData(object):
             Part(7000, 3200), Part(7000, 3200), Part(3620, 2900), Part(3620, 2900), Part(7000, 374),
             Part(6920, 500), Part(6920, 500)
         ]
+
+    @staticmethod
+    def get_unterschrank_4():
+        return [
+            Part(7620, 5800), Part(7620, 5800), Part(5620, 800), Part(5620, 5600), Part(5620, 5600),
+            Part(7620, 5740), Part(5960, 1575), Part(4800, 655), Part(4915, 5300),
+        ]
+
+    @staticmethod
+    def get_traverse(count=1):
+        traverse = Part(5620, 800)
+        return list(itertools.repeat(traverse, count))
