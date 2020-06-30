5f7e0b187d8bf84631d0231eea2341fdefe5ec85
Martin Neumayer <neumayer@isse.de>
2020-06-30 17:32:43 +0200

    Refactor BestFitDecreasing heuristic, still buggy

--- src/Heuristics/BestFitDecreasing.py
+++ src/Heuristics/BestFitDecreasing.py
@@ -10,30 +10,39 @@ class BestFitDecreasing(Runner):
     def run(self, data):
         parts = data["parts"]
         modules = data["modules"]
+        # TODO: Add exacter timer
         start = datetime.now()
         solution = Solution()
+        solution.allocation = [None] * len(parts)
         sorted_parts = sorted(parts, key=lambda part: part.width * part.length, reverse=True)
 
         for part in sorted_parts:
-            # Filter out all modules, that the part won't fit in
-            filtered_modules = filter(
-                lambda module: (module.length >= part.length and module.width + module.jut >= part.width)
-                               or (module.length >= part.width and module.width + module.jut >= part.length), modules)
-            # Calculate the wasted space per module
-            wasted_space = list(
-                map(lambda module: module.length * (module.width + module.jut) - part.width * part.length,
-                    filtered_modules))
-            # Get the minimum wasted space, that is greater or equal to zero
-            least_wasted_space = min(i for i in wasted_space if i >= 0)
-            best_fit = wasted_space.index(least_wasted_space)
-            # TODO: Undo sorting!
-            solution.allocation.append(best_fit)
-            solution.wasted_space_sum = solution.wasted_space_sum + least_wasted_space
-            if modules[best_fit].capacity == 1:
-                modules.remove(modules[best_fit])
+            best_fit, wasted_space = self.get_best_fitting_module(part, modules)
+            index_of_best_fit = modules.index(best_fit)
+            solution.allocation[parts.index(part)] = index_of_best_fit
+            solution.wasted_space_sum = solution.wasted_space_sum + wasted_space
+            if modules[index_of_best_fit].capacity == 1:
+                modules.remove(modules[index_of_best_fit])
             else:
-                modules[best_fit].capacity = modules[best_fit].capacity - 1
+                modules[index_of_best_fit].capacity = modules[index_of_best_fit].capacity - 1
 
         stop = datetime.now()
         statistics = {"time": stop - start}
         return Result(Status.SATISFIED, solution, statistics)
+
+    def get_best_fitting_module(self, part, modules):
+        # Filter out all modules, that the part won't fit in
+        feasible_modules = filter(lambda module: self.part_fits_in_module(part, module), modules)
+        modules_to_wasted_space = {}
+        for module in feasible_modules:
+            wasted_space = self.calculate_wasted_space(part, module)
+            modules_to_wasted_space[module] = wasted_space
+        result = min(modules_to_wasted_space, key=modules_to_wasted_space.get)
+        return result, modules_to_wasted_space[result]
+
+    def part_fits_in_module(self, part, module):
+        return (module.length >= part.length and module.width + module.jut >= part.width) or (
+                module.length >= part.width and module.width + module.jut >= part.length)
+
+    def calculate_wasted_space(self, part, module):
+        return module.length * (module.width + module.jut) - part.width * part.length
