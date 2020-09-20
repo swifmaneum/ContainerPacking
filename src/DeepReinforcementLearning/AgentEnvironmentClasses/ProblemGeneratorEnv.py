d8483d340c2147b99e720d489896d10016b9bbac
Martin Neumayer <neumayer@isse.de>
2020-09-20 12:08:26 +0200

    Add a new Env based on our generally used input data

---
+++ src/DeepReinforcementLearning/AgentEnvironmentClasses/ProblemGeneratorEnv.py
@@ -0,0 +1,78 @@
+import copy
+
+import gym
+import numpy as np
+from gym import spaces
+from gym.utils import seeding
+
+from DeepReinforcementLearning.AgentEnvironmentClasses.aiRuleBase import is_best_fitting_module
+
+
+class ProblemGeneratorEnv(gym.Env):
+
+    def __init__(self, problem_data):
+        self.problem_data = problem_data
+        self.parts = copy.deepcopy(self.problem_data["parts"])
+        self.modules = copy.deepcopy(self.problem_data["modules"])
+        self.part = None
+        self.current_part_index = 0
+        self.reward = 0
+        self.next_environment_observation = ()
+        # Allow the length of modules as actions, + 1 for 'no module available'
+        self.action_space = spaces.Discrete(len(self.modules) + 1)
+
+        observation_space_low = np.array([40, 40])
+        observation_space_high = np.array([2000, 600])
+        for module in self.modules:
+            # Add the minimum capacity 0 for every module
+            observation_space_low = np.append(observation_space_low, 0)
+            # Add the actual capacity for every module
+            observation_space_high = np.append(observation_space_high, module.capacity)
+        self.observation_space = spaces.Box(low=observation_space_low, high=observation_space_high, dtype=np.uint64)
+        self.np_random = ()
+        self.seed()
+        self.reset()
+
+    def seed(self, seed=None):
+        self.np_random, seed = seeding.np_random(seed)
+        return [seed]
+
+    def step(self, action):
+        assert self.action_space.contains(action)
+        # Decrease the action value (1-indexed) by one to map it to python list indices (0-indexed)
+        action_index = action - 1
+        #print(action_index)
+
+        # If we did not choose 'no module available'
+        if action != len(self.modules) + 1:
+            # Decrease the capacity of the chosen module
+            self.modules[action_index].capacity = self.modules[action_index].capacity - 1
+
+        if is_best_fitting_module(action_index, self.part, self.modules):
+            self.reward = 1
+        else:
+            self.reward = 0
+
+        self.current_part_index = self.current_part_index + 1
+        if self.current_part_index == len(self.parts):
+            # If we've reached the last part, end the episode by returning done is true
+            return np.empty(9), self.reward, True, {}
+        else:
+            # Else return the next part and continue the episode
+            self.part = self.parts[self.current_part_index]
+            self.next_environment_observation = self.build_observation(self.part)
+            return self.next_environment_observation, self.reward, False, {}
+
+    def reset(self):
+        self.modules = copy.deepcopy(self.problem_data["modules"])
+        self.current_part_index = 0
+        self.part = self.parts[self.current_part_index]
+        self.next_environment_observation = self.build_observation(self.part)
+        return self.next_environment_observation
+
+    def build_observation(self, part):
+        part_dimensions = (part.length, part.width)
+        module_capacities = []
+        for module in self.modules:
+            module_capacities.append(module.capacity)
+        return np.array(part_dimensions + tuple(module_capacities))
--- src/DeepReinforcementLearning/AgentEnvironmentClasses/aiRuleBase.py
+++ src/DeepReinforcementLearning/AgentEnvironmentClasses/aiRuleBase.py
@@ -24,3 +24,21 @@ def get_best_fitting_module(nn_decision, part, modules):
             return 1
         else:
             return 0
+
+
+def is_best_fitting_module(decision, part, modules) -> bool:
+    """
+    Returns a boolean whether a given decision for a given part is the best fitting module.
+    If no fitting modules is available, the decision len(modules) + 1 will return true.
+    """
+    feasible_modules = list(filter(lambda m: part_fits_in_module(part, m) and m.capacity > 0, modules))
+    modules_to_wasted_space = {}
+    for module in feasible_modules:
+        wasted_space = calculate_wasted_space(part, module)
+        modules_to_wasted_space[module] = wasted_space
+    if len(modules_to_wasted_space) == 0:
+        # If there is no feasible module, the output should be the len of modules +1, indicating 'no module available'
+        return decision == len(modules) + 1
+    else:
+        best_fitting_module = min(modules_to_wasted_space, key=modules_to_wasted_space.get)
+        return decision == modules.index(best_fitting_module)
