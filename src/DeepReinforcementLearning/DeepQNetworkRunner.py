4c71c44ba4103077daa0e38d3e0907a808045546
Martin Neumayer <neumayer@isse.de>
2020-11-10 11:16:35 +0100

    Getting rid of the mostly duplicated second Environment, now training and evaluation is possible in Environment.py. Decision whether training or evaluating is based on the constructor parameter problem_data. Also removing plotting for learning procedure

--- src/DeepReinforcementLearning/DeepQNetworkRunner.py
+++ src/DeepReinforcementLearning/DeepQNetworkRunner.py
@@ -7,15 +7,14 @@ from keras.models import Sequential
 from rl.policy import EpsGreedyQPolicy
 from rl.memory import SequentialMemory
 from keras.layers import Dense, Activation, Flatten
-from DeepReinforcementLearning.AgentEnvironmentClasses.TrainingEnv import TrainingEnv
+from DeepReinforcementLearning.AgentEnvironmentClasses.Environment import Environment
 from DeepReinforcementLearning.AgentEnvironmentClasses.aiRuleBase import part_fits_in_module
-from DeepReinforcementLearning.AgentEnvironmentClasses.EvaluationEnv import EvaluationEnv
 
 
 class DeepQNetworkRunner(Runner):
 
     def __init__(self):
-        self.environment = TrainingEnv()
+        self.environment = Environment()
 
         np.random.seed(1)
         self.environment.seed(1)
@@ -45,25 +44,14 @@ class DeepQNetworkRunner(Runner):
         self.dqn.load_weights('DeepReinforcementLearning/AgentEnvironmentClasses/Weights/dqn_sorting_weights.h5f')
 
     def train(self):
-        self.environment = TrainingEnv()
+        self.environment = Environment()
         steps = 100000
-        window_size = 100
-        points_to_plot = []
-
-        history = self.dqn.fit(self.environment, nb_steps=steps, visualize=False, verbose=2)
-        for i in range(0, steps):
-            points_to_plot.append(np.mean(history.history['episode_reward'][i * window_size:(i + 1) * window_size]))
-        from matplotlib import pyplot as plt
-        plt.plot(points_to_plot)
-        plt.title('avg reward over time')
-        plt.ylabel('average reward')
-        plt.xlabel('epoch')
-        plt.show()
+        self.dqn.fit(self.environment, nb_steps=steps, visualize=False, verbose=2)
         self.dqn.save_weights('DeepReinforcementLearning/AgentEnvironmentClasses/Weights/dqn_sorting_weights.h5f',
                               overwrite=True)
 
     def find_solution(self, data):
-        self.environment = EvaluationEnv(data)
+        self.environment = Environment(data)
         self.dqn.test(self.environment, nb_episodes=1, visualize=False)
         if self.is_feasible(self.environment.solution):
             return self.environment.solution
