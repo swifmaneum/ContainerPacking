794ee70e48ddb24ed26c1a198cea5e8a0f1f5a07
Martin Neumayer <neumayer@isse.de>
2020-11-10 13:00:50 +0100

    Remove console output of keras-rl when evaluating

--- src/Starter.py
+++ src/Starter.py
@@ -27,10 +27,10 @@ for model_runner, model_name in algorithms_to_test:
 
     min_number_of_containers = 1
 
-    problem_generator = RandomProblemGenerator(1250)  # 1250/2500 works for 100 / 5000 works for 80 boards
+    problem_generator = RandomProblemGenerator(2500)  # 1250/2500 works for 100 / 5000 works for 80 boards
 
     parts = []
-    for i in range(1, 10):
+    for i in range(1, 200):
 
         parts = parts + next(problem_generator)
 
